6e8b0baf754ce777aaa2f2911cff5b8ad4e5a43a
AyahAshkanani <ayah.ash62@gmail.com>
2021-07-04 16:17:52 +0300

    almost done

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,16 +1,43 @@
 import { TextField, MenuItem } from "@material-ui/core";
 import { AddTaskButtonStyled } from "../styles";
+import { useState } from "react";
+import taskStore from "../stores/taskStore";
 
 const AddTask = (props) => {
+  const [task,setTask] = useState(
+    {
+        title:"",
+        tag:"none",
+        priority:"none",
+    });
+
+const handleChange = (event) => {
+  setTask({ ...task, [event.target.name]: event.target.value });
+
+};
+
+const handleSubmit = (event) => {
+event.preventDefault();
+ taskStore.taskCreate(task);
+// setTask({
+  // title:"",
+  // tag:"none",
+  // priority:"none",
+// });
+};
+
+
   const priorities = ["high", "low", "none"];
   const tags = ["work", "home", "none"];
   return (
     <div>
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
         <TextField
           id="standard-basic"
           label="Add new task"
           style={{ width: "74%", marginLeft: 55 }}
+          onChange={handleChange}
+          name="title"
         />
 
         <TextField
@@ -19,8 +46,7 @@ const AddTask = (props) => {
           select
           label="Tag"
           style={{ width: "7.9%", color: "#C2C2C2" }}
-          //   onChange={handleChange}
-          //   helperText="tag"
+          onChange={handleChange}
         >
           {tags.map((option) => (
             <MenuItem key={option} value={option}>
@@ -34,6 +60,7 @@ const AddTask = (props) => {
           select
           label="Priority"
           style={{ width: "7.9%" }}
+          onChange={handleChange}
         >
           {priorities.map((option) => (
             <MenuItem key={option} value={option}>
